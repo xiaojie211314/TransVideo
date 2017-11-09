84b608a4b53bf74bb4bf3725f527fd04af10bfa8
于志远 <lhyfe1987@163.com>
2017-11-09 13:31:57 +0800

    添加七牛是否是视频的判断

--- src/main/java/com/qiniu/util/Token.java
+++ src/main/java/com/qiniu/util/Token.java
@@ -12,7 +12,7 @@ public class Token {
     private String encodedSign;
     private String encodedPutPolicy;
 
-    public Token(String token, String secretkey){
+    public Token(String token, String secretkey) {
         this.secretKey = secretkey;
         String[] result = token.split(":");
         this.accessKey = result[0];
@@ -23,28 +23,40 @@ public class Token {
     /**
      * 得到token里面保存的json字符串
      */
-    public JSONObject getPutPolicy(){
+    public JSONObject getPutPolicy() {
 
         String res = new String(UrlSafeBase64.decode(this.encodedPutPolicy));
         JSONObject jsonObject = JSONObject.parseObject(res);
+        if (jsonObject.containsKey("persistentOps")) {
+            //如果是视频转码，处理视频转码中的Saveas方法
+            String[] persitentString = jsonObject.getString("persistentOps").split("|");
+            if (persitentString.length == 2) {
+                //如果有saveas 的方法,从saveas中获取转码之后的文件名
+                String[] saveas = new String(UrlSafeBase64.decode(persitentString[1].replace("saveas/", "")))
+                        .split(":");
+                if (saveas.length == 2) {
+                    jsonObject.put("outkey", saveas[1]);
+                }
+            }
+            jsonObject.put("outkey", "");
+        }
         return jsonObject;
     }
-    
+
     /**
      * 检查sign是否下砸 
      */
     public boolean isValidCallback(String accessKey) {
         //根据encodedPutPolicy 使用hmac_sha1进行签名
-        String sign="";
-		try {
-			sign = Hmacsha1.getSignature(this.encodedPutPolicy, this.secretKey);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        String sign = "";
+        try {
+            sign = Hmacsha1.getSignature(this.encodedPutPolicy, this.secretKey);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         String encodedSign = UrlSafeBase64.encodeToString(sign);
         return encodedSign.equals(this.encodedSign) && accessKey.equals(this.accessKey);
     }
 
-    
 }
