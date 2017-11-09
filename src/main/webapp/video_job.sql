21005376e3263408d64f62657488b17e084eed28
xiaojie211314 <lijie@soocedu.com>
2017-11-09 18:06:58 +0800

    修改转码参数接口，数据库

--- src/main/webapp/video_job.sql
+++ src/main/webapp/video_job.sql
@@ -30,8 +30,8 @@ CREATE TABLE `video_job` (
   `status` int(11) NOT NULL DEFAULT '0' COMMENT '视频状态 0:等待转码中 1:转码进行中 2:转码成功 3:转码失败 4:转码成功回调失败',
   `counts` int(11) NOT NULL DEFAULT '0' COMMENT '转码次数 最多转3次',
   `msg` varchar(1000) DEFAULT '' COMMENT '转码错误信息，如果有错误信息就写入',
-  `callmsg` varchar(1000) DEFAULT '' COMMENT '回调错误信息，返回接收到回调接口的信息',
-  `videokey` varchar(100) NOT NULL DEFAULT '' COMMENT '视频唯一值,uuid 格式,用来转码成功后告知服务器更新哪个视频',
+  `error` varchar(1000) DEFAULT '' COMMENT '回调错误信息，返回接收到回调接口的信息',
+  `persistentid` varchar(100) NOT NULL DEFAULT '' COMMENT '视频唯一值,uuid 格式,用来转码成功后告知服务器更新哪个视频',
   `inputtime` bigint(20) NOT NULL DEFAULT '0' COMMENT '视频插入时间',
   `updatetime` bigint(20) NOT NULL DEFAULT '0' COMMENT '视频更新时间（开始转码，转码成功或转码失败）',
   PRIMARY KEY (`id`)
