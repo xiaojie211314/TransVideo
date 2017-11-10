01af596ee6bd35309d59b1cacd6ea19f311b96c4
xiaojie211314 <lijie@soocedu.com>
2017-11-10 12:11:03 +0800

    转码根目录调整

--- src/main/java/com/soocedu/util/Const.java
+++ src/main/java/com/soocedu/util/Const.java
@@ -4,7 +4,7 @@ package com.soocedu.util;
  * Created by jieli on 2017/10/17.
  */
 public interface Const {
-    public final static String  WEBROOT= System.getProperty("webapp.root");
+    public final static String  WEBROOT= System.getProperty("webapp.video.root");
 
     public final static  String  SECRET_KEY = "FXeoUwFhWShmpttTdNx74YIf1-tS-6O6enetIySN";
     public final static  String  ACCESS_KEY = "HC5_GylOBVm8AY_XXFDD5Lkk0MiW7p2tjPG9_1o1";
